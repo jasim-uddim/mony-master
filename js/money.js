f96c805356060f7442a56fe5f0155727796fda92
unknown <jasimuddin25635@gmail.com>
2022-02-17 16:43:59 +0600

    added error

--- js/money.js
+++ js/money.js
@@ -1,11 +1,18 @@
 // common function
 
 function getValue(inputId, isInput) {
+  const inputValue = document.getElementById(inputId);
   if (isInput) {
-    const inputAmound = parseFloat(document.getElementById(inputId).value);
-    return inputAmound;
+    if (isNaN(inputValue.value) || inputValue.value < 0) {
+      alert(
+        `Error in ${inputId}: give positive number in ${inputId} please!!!`
+      );
+    } else {
+      const inputAmound = parseFloat(inputValue.value);
+      return inputAmound;
+    }
   } else {
-    const inputAmound = parseFloat(document.getElementById(inputId).innerText);
+    const inputAmound = parseFloat(inputValue.innerText);
     return inputAmound;
   }
 }
@@ -22,28 +29,32 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
   const clothesExpenses = getValue("clothes-expenses", true);
 
   const totalExpenses = foodExpenses + rentExpenses + clothesExpenses;
-  //   Total expenses update
-  document.getElementById("total-expenses").innerText = totalExpenses;
-  //   Total Balance update
+  if (totalIncome > totalExpenses) {
+    //   Total expenses update
+    document.getElementById("total-expenses").innerText = totalExpenses;
+    //   Total Balance update
 
-  document.getElementById("total-balance").innerText =
-    totalIncome - totalExpenses;
+    document.getElementById("total-balance").innerText =
+      totalIncome - totalExpenses;
+  } else {
+    alert("Increase income");
+  }
 });
 // event handaler
 document.getElementById("save-btn").addEventListener("click", function () {
   const totalIncome = getValue("total-income", true);
   const balance = getValue("total-balance", false);
   // save input
-  const saveInput = document.getElementById("save-input");
+  const saveInput = getValue("save-input", true);
+  const saveAmount = (totalIncome * saveInput) / 100;
 
-  const saveValue = parseFloat(saveInput.value);
-  // saving amound
-  const saveAmound = (totalIncome * saveValue) / 100;
-  const saveMoney = document.getElementById("saveing-amound");
-  const saveText = saveMoney.innerText;
-  saveMoney.innerText = saveAmound;
-  // Remaining Balance
-  const remainedBalance = document.getElementById("remaining-balance");
-  const remainedBalanceText = remainedBalance.innerText;
-  remainedBalance.innerText = balance - saveAmound;
+  if (saveAmount < balance) {
+    // Update saving amount
+    document.getElementById("saveing-amound").innerText = saveAmount;
+    // Update Remaining Balance
+    document.getElementById("remaining-balance").innerText =
+      balance - saveAmount;
+  } else {
+    alert("insufficient balance");
+  }
 });
